3307eb5ca704b94dfbad3fdc5d3396ee16cb5884
hemavenkat <hemavenkat59@gmail.com>
2018-02-19 16:48:45 +0530

    Finished Project

---
+++ DrawingGraphics/DrawGraphics.podspec
@@ -0,0 +1,16 @@
+
+
+Pod::Spec.new do |s|
+  s.name         = "DrawGraphics"
+  s.version      = "1.0.0"
+  s.summary   = "This framework allows user to create shapes"
+  s.homepage     = "https://github.com/hema1959/DrawGraphics.git"
+  s.license      = "MIT"
+  s.author             = { "hemavenkat" => "hemavenkat59@gmail.com" }
+  s.platform     = :ios, "11.0"
+  s.source       = { :git => "https://github.com/hema1959/DrawGraphics.git", :tag => "1.0.0" }
+  s.source_files  = "DrawGraphics/**"
+  s.framework  = "UIKit"
+  
+
+end
---
+++ DrawingGraphics/DrawingGraphics/DrawingGraphics.h
@@ -0,0 +1,19 @@
+//
+//  DrawingGraphics.h
+//  DrawingGraphics
+//
+//  Created by HEMA VENKATARAMAN on 19/02/2018.
+//  Copyright © 2018 HEMA VENKATARAMAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawingGraphics.
+FOUNDATION_EXPORT double DrawingGraphicsVersionNumber;
+
+//! Project version string for DrawingGraphics.
+FOUNDATION_EXPORT const unsigned char DrawingGraphicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawingGraphics/PublicHeader.h>
+
+
